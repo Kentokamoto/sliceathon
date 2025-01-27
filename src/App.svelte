e2d07d2f89e0fa3d0318b82d1702e02a31252203
Kento Okamoto <kentokamoto@proton.me>
2025-01-26 23:59:55 -0800

    Cleanup

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
-    import svelteLogo from "./assets/svelte.svg";
-    import viteLogo from "/vite.svg";
-    import Title from "./lib/Title.svelte";
-    import "./app.css";
+  import Title from "./lib/Title.svelte";
+  import "./app.css";
 </script>
 
 <main class="bg-crust w-full place-content-center">
-    <div class="w-full place-content-center">
-        <Title />
-    </div>
+  <div class="w-full place-content-center">
+    <Title />
+  </div>
 </main>
