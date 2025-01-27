d27dfcaa1be48c0934083f09e82ff467a8393344
Kento Okamoto <kentokamoto@proton.me>
2025-01-26 22:26:51 -0800

    fix styling for smaller screens

--- src/lib/Title.svelte
+++ src/lib/Title.svelte
@@ -3,17 +3,17 @@
 
 <div class="bg-yellow-cheese flex flex-row w-full">
   <i
-    class="fa-solid fa-pizza-slice text-4xl place-content-center text-red-sauce p-8 flex-none"
+    class="fa-solid fa-pizza-slice text-xl sm:text-4xl place-content-center text-red-sauce p-4 sm:p-8 flex-none"
   ></i>
   <div class="flex-1 place-content-center">
-    <h2 class="text-3xl text-red-sauce text-center font-extrabold">
+    <h2 class="text-xl sm:text-3xl text-red-sauce text-center font-extrabold">
       First Annual
     </h2>
-    <h1 class="text-4xl text-red-sauce text-center font-black">
+    <h1 class="text-2xl sm:text-4xl text-red-sauce text-center font-black">
       Seattle Slice-A-Thon
     </h1>
   </div>
   <i
-    class="fa-solid fa-pizza-slice text-4xl place-content-center text-red-sauce p-8 flex-none"
+    class="fa-solid fa-pizza-slice text-xl sm:text-4xl place-content-center text-red-sauce p-4 sm:p-8 flex-none"
   ></i>
 </div>
