4f24fe6d72d33b798e17a23d9c5c1f7000b46a05
Kento Okamoto <kentokamoto@proton.me>
2025-01-26 21:27:23 -0800

    Slice-a-thon page with fonts and prettier for formatting

--- src/lib/Title.svelte
+++ src/lib/Title.svelte
@@ -1,8 +1,19 @@
-<div class="bg-yellow-cheese">
-    <h2 class="text-3xl text-red-sauce text-center font-extrabold">
-        First Annual
-    </h2>
-    <h1 class="text-4xl text-red-sauce text-center font-black">
-        Seattle Slice-A-Thon
-    </h1>
+<script>
+</script>
+
+<div class="bg-yellow-cheese flex flex-row w-full">
+    <i
+        class="fa-solid fa-pizza-slice text-4xl place-content-center p-8 flex-none"
+    ></i>
+    <div class="flex-1 place-content-center">
+        <h2 class="text-3xl text-red-sauce text-center font-extrabold">
+            First Annual
+        </h2>
+        <h1 class="text-4xl text-red-sauce text-center font-black">
+            Seattle Slice-A-Thon
+        </h1>
+    </div>
+    <i
+        class="fa-solid fa-pizza-slice text-4xl place-content-center p-8 flex-none"
+    ></i>
 </div>
