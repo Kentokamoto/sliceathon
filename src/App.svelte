bcce6be7d405fbda975eb7c5bd51e4bf57dd2f95
Kento Okamoto <kentokamoto@proton.me>
2025-01-27 21:25:14 -0800

    Add basic details about course

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
   import Title from "./lib/Title.svelte";
+  import Details from "./lib/Details.svelte";
   import "./app.css";
 </script>
 
 <main class="bg-crust w-full place-content-center">
   <div class="w-full place-content-center">
     <Title />
+    <Details />
   </div>
 </main>
---
+++ src/lib/Details.svelte
@@ -0,0 +1,28 @@
+<div class="flex flex-col sm:flex-row place-content-center">
+  <div class="basis-full flex flex-col place-content-center">
+    <h1 class="text-xl font-bold text-basil text-center sm:pl-2">
+      Race Details
+    </h1>
+    <div class="basis-full text-basil p-2 sm:p-10">
+      <h2 class="text-l font-bold text-center sm:text-left">Date</h2>
+      <p class="text-center sm:text-left">TBD</p>
+      <h2 class="text-l font-bold text-center sm:text-left">Start Time</h2>
+      <p class="text-center sm:text-left">TBD</p>
+      <h2 class="text-l font-bold text-center sm:text-left">Location</h2>
+      <p class="text-center sm:text-left">Gasworks Park</p>
+    </div>
+  </div>
+
+  <div class="basis-full flex place-content-center p-4 sm:p-0">
+    <div class="">
+      <h1 class="text-xl text-basil font-bold text-center">Course Route</h1>
+      <iframe
+        title="Slice-a-thon Route"
+        src="https://routes.rungoapp.com/embedded/F6oUp8z73Y?units=miles&hasTurns=false&hasPOIs=true&hideMetrics=false&isStatic=true"
+        width="375px"
+        height="550px"
+        name="Slice-a-Thon "
+      ></iframe>
+    </div>
+  </div>
+</div>
