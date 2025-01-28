3b22306d8dd8531c995da9c2c5866313f878ae36
Kento Okamoto <kentokamoto@proton.me>
2025-01-27 21:44:32 -0800

    Add counter

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
   import Title from "./lib/Title.svelte";
   import Details from "./lib/Details.svelte";
+  import Counter from "./lib/Counter.svelte";
   import "./app.css";
 </script>
 
 <main class="bg-crust w-full place-content-center">
   <div class="w-full place-content-center">
     <Title />
+    <Counter />
     <Details />
   </div>
 </main>
---
+++ src/lib/Counter.svelte
@@ -0,0 +1,41 @@
+<script>
+  const judgementDate = new Date("April 20, 2025 10:00:00").getTime(); // This is not the final date
+  let days = 0;
+  let hours = 0;
+  let minutes = 0;
+  let seconds = 0;
+  let x = setInterval(() => {
+    const currDateTime = new Date().getTime();
+    const diff = judgementDate - currDateTime;
+    days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+    seconds = Math.floor((diff % (1000 * 60)) / 1000);
+  }, 1000);
+</script>
+
+<div>
+  <div
+    class="flex flex-row bg-red-sauce text-yellow-cheese place-content-center py-2"
+  >
+    <div class="px-10 text-center">
+      <h1>{days}</h1>
+      <h1>Days</h1>
+    </div>
+    <div class="px-10 text-center">
+      <h1>{hours}</h1>
+      <h1>Hours</h1>
+    </div>
+    <div class="px-10 text-center">
+      <h1>{minutes}</h1>
+      <h1>Minutes</h1>
+    </div>
+    <div class="px-10 text-center">
+      <h1>{seconds}</h1>
+      <h1>Seconds</h1>
+    </div>
+  </div>
+  <p class="font-bold bg-red-sauce text-yellow-cheese text-xl text-center">
+    DATE is still TBD regardless of Timer
+  </p>
+</div>
