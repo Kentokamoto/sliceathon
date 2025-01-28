9e2603dcca5afc56fb352ea6261063cb32d94ca9
Kento Okamoto <kentokamoto@proton.me>
2025-01-27 21:48:53 -0800

    Fix smaller screens

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -18,24 +18,26 @@
   <div
     class="flex flex-row bg-red-sauce text-yellow-cheese place-content-center py-2"
   >
-    <div class="px-10 text-center">
+    <div class="px-2 sm:px-10 text-center">
       <h1>{days}</h1>
       <h1>Days</h1>
     </div>
-    <div class="px-10 text-center">
+    <div class="px-2 sm:px-10 text-center">
       <h1>{hours}</h1>
       <h1>Hours</h1>
     </div>
-    <div class="px-10 text-center">
+    <div class="px-2 sm:px-10 text-center">
       <h1>{minutes}</h1>
       <h1>Minutes</h1>
     </div>
-    <div class="px-10 text-center">
+    <div class="px-2 sm:px-10 text-center">
       <h1>{seconds}</h1>
       <h1>Seconds</h1>
     </div>
   </div>
-  <p class="font-bold bg-red-sauce text-yellow-cheese text-xl text-center">
+  <p
+    class="font-bold uppercase bg-red-sauce text-yellow-cheese text-l sm:text-xl text-center"
+  >
     DATE is still TBD regardless of Timer
   </p>
 </div>
