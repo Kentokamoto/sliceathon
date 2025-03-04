aa91e21855fe347532d966e9447e6ffd1c5401f3
Kento Okamoto <kentokamoto@proton.me>
2025-03-03 21:18:54 -0800

    starting faq section

---
+++ src/lib/Accordion.svelte
@@ -0,0 +1,21 @@
+<script>
+  let { title, content } = $props();
+  let isOpen = $state(false);
+  function onclick() {
+    isOpen = !isOpen;
+  }
+</script>
+
+<div>
+  <button {onclick} class="flex w-full flex-row">
+    <div class="flex-15 text-left">
+      {title}
+    </div>
+    <div class="flex-1">{isOpen ? "-" : "+"}</div>
+  </button>
+  {#if isOpen}
+    <div>
+      {content}
+    </div>
+  {/if}
+</div>
---
+++ src/lib/FAQ.svelte
@@ -0,0 +1,10 @@
+<script>
+  import Accordion from "./Accordion.svelte";
+</script>
+
+<div class="flex flex-col place-content-center sm:flex-row">
+  <div class="flex basis-full flex-col">
+    <h1 class="text-basil text-xl font-bold sm:pl-2">FAQ</h1>
+    <Accordion title={"who am i"} content={"a runner"} />
+  </div>
+</div>
