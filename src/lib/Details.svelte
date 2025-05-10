fb3f0691d6add7eb9b9f9cb3f8630211d99272eb
Kento Okamoto <kentokamoto@proton.me>
2025-05-09 19:06:53 -0700

    Update Slice-a-thon route

--- src/lib/Details.svelte
+++ src/lib/Details.svelte
@@ -15,12 +15,14 @@
     class="flex flex-3 basis-full flex-col place-content-center p-4 sm:p-0"
   >
     <h1 class="text-red-sauce text-center text-2xl font-bold">Course Route</h1>
-    <iframe
-      title="Slice-a-thon route"
-      class="place-content-center rounded-lg"
-      src="https://routes.rungoapp.com/embedded/F6oUp8z73Y?units=miles&hasTurns=false&hasPOIs=true&hideMetrics=false&isStatic=true"
-      height="650px"
-      name="Slice-a-Thon "
-    ></iframe>
+    <div
+      class="strava-embed-placeholder"
+      data-embed-type="route"
+      data-embed-id="3356103777858949274"
+      data-style="standard"
+      data-map-hash="11.05/47.6304/-122.3255/0/3"
+      data-from-embed="true"
+    ></div>
+    <script src="https://strava-embeds.com/embed.js"></script>
   </section>
 </div>
