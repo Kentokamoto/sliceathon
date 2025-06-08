ed251373aa607a19baadbacc3d1ed0155ffc6dfe
Kento Okamoto <kentokamoto@proton.me>
2025-06-08 14:13:42 -0700

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -9,7 +9,9 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-			fallback: '404.html'
+			pages: 'build',
+			assets: 'build',
+			fallback: null
 		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
