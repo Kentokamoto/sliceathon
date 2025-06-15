0c166af59acb8c5f8debec23982d4ab06598a0b2
Kento Okamoto <kentokamoto@proton.me>
2025-06-14 23:27:59 -0700

    Set floor to be 0

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -6,7 +6,7 @@
   let seconds = 0;
   let x = setInterval(() => {
     const currDateTime = new Date().getTime();
-    const diff = judgementDate - currDateTime;
+    const diff = Math.max(judgementDate - currDateTime,0);
     days = Math.floor(diff / (1000 * 60 * 60 * 24));
     hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
