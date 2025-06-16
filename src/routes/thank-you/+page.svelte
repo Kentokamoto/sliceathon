d324770edf4a46901bfd89efda7dcc1efa394121
Kento Okamoto <kentokamoto@proton.me>
2025-06-16 13:21:11 -0700

    Start thank you page

---
+++ src/routes/thank-you/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import groupPhoto from '$lib/assets/slice-a-thon_group_photo.webp';
+</script>
+
+<div class="my-5 place-items-center">
+	<img class="my-4" src={groupPhoto} alt="group" />
+	<h1 class="text-red-sauce text-center text-3xl">Thank you!</h1>
+</div>
