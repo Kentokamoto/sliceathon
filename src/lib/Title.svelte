fa3550ade4b66773028dccf39131f0dd528d126c
Kento Okamoto <kentokamoto@proton.me>
2025-06-16 13:20:41 -0700

    Update page to use slice-a-thon mascot

--- src/lib/Title.svelte
+++ src/lib/Title.svelte
@@ -1,20 +1,25 @@
 <script>
 	import { base } from '$app/paths';
+	import mascotThumb from '$lib/assets/Slice-a-Thon_Mascot_thumb.webp';
 </script>
 
 <a href="{base}/">
 	<header class="bg-yellow-cheese flex w-full flex-row">
-		<i
-			class="fa-solid fa-pizza-slice text-red-sauce flex-none place-content-center p-4 text-xl sm:p-8 sm:text-4xl"
-		></i>
+		<img
+			class="m-4 w-8 flex-none place-content-center object-scale-down"
+			src={mascotThumb}
+			alt="slice-a-thon mascot"
+		/>
 		<div class="flex-1 place-content-center">
 			<h2 class="text-red-sauce text-center text-xl font-extrabold sm:text-3xl">First Annual</h2>
 			<h1 class="text-red-sauce text-center text-2xl font-black sm:text-4xl">
 				Seattle Slice-A-Thon
 			</h1>
 		</div>
-		<i
-			class="fa-solid fa-pizza-slice text-red-sauce flex-none place-content-center p-4 text-xl sm:p-8 sm:text-4xl"
-		></i>
+		<img
+			class="m-4 w-8 flex-none place-content-center object-scale-down"
+			src={mascotThumb}
+			alt="slice-a-thon mascot"
+		/>
 	</header>
 </a>
